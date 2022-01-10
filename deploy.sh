00bd7efd39723d8aa247696fe3be8ec7cd212b77
Giang Nguyen <giangnguyen@magrabbit.com>
2022-01-10 10:41:11 +0700

    files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-flutter build web --no-sound-null-safety
-rm -rf ./public
-cp -r ./build/web ./public
+rm ./public/main.dart.js
+cp ./build/web/main.dart.js ./public
+git add .
+git commit -m 'files'
+git push
